d37ba2899ffe52c8ea2b9bd457781c21480d73c5
Rafal Dittwald <rafal.dittwald@gmail.com>
2017-09-28 10:28:50 +0200

    Update project settings in preparation for clojars release

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject merge-insertion-sort "1.0.0"
-  
+(defproject decidedlyso/merge-insertion-sort "1.0.0"
+
+  :description "Implementation of the comparison-efficient Merge Insertion Sort / Ford-Johnson Algorithm"
+
+  :url "https://github.com/decidedlyso/merge-insertion-sort"
+
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  
+
   :main merge-insertion-sort.core)
